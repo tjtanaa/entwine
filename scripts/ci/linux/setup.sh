90758217c62e605bc358569899f05abc5c30421d
Howard Butler <howard@hobu.co>
2020-10-02 12:56:50 -0500

    react to failure

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -9,7 +9,7 @@ conda install cmake ninja compilers -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet pdal=2.2.0=hc76372f_3 abseil-cpp  -y
+    conda install --yes --quiet pdal=2.2.0=h9463286_0 abseil-cpp  -y
     conda install --yes --quiet entwine  --only-deps -y
 
 else
